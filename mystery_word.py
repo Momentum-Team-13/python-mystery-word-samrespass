04ea6427f06d78e5398ca1ce2ed07f33ce15ec7c
Sam Respass <samrespass@gmail.com>
2022-06-21 18:12:57 -0400

    Still working on multiple letter bug

--- mystery_word.py
+++ mystery_word.py
@@ -1,5 +1,50 @@
+import random
+import operator
+import os
+import sys
+
+words = open("words.txt", "r")
+words_two = words.read()
+word_bank = words_two.split()
+guess_me = random.choice(word_bank)
+breaker = list(guess_me)
+str = ''
+new_board = "_" * len(breaker)
+scoreboard = list(new_board)
+WRONG = []
+past_guesses = []
+
 def play_game():
-    pass
+    print(f"{str.join(scoreboard)} \n GUESS ME!")
+    print(guess_me)
+    if len(WRONG) == 8:
+        print(f"LOSER!\nThe Word Was: {guess_me}")
+        sys.exit(0)
+    if scoreboard == breaker:
+        print("YOU WIN!")
+        sys.exit(0)
+    guess = input("Pick A Letter: ")
+    if guess[0] in past_guesses:
+        print("You Already Tried That...")
+        play_game()
+    elif guess[0] in breaker:
+        print("good guess!")
+        past_guesses.append(guess[0])
+        guess_list = list(guess[0])
+        for correct_guess in range(len(breaker)):
+            print(correct_guess)
+            # correct = breaker.index(guess[0])
+            # scoreboard[correct] = breaker[correct]
+        play_game()
+    elif guess[0] not in breaker:
+        print("dumbass")
+        past_guesses.append(guess[0])
+        WRONG.append("X")
+        print("X" * len(WRONG))
+        play_game()
+
+
+
 
 
 if __name__ == "__main__":
