218397d6ec485dec9447b7b02e14159a55cbeb65
Sam Respass <samrespass@gmail.com>
2022-06-21 22:46:52 -0400

    QOL features

--- mystery_word.py
+++ mystery_word.py
@@ -8,38 +8,42 @@ words_two = words.read()
 word_bank = words_two.split()
 guess_me = random.choice(word_bank)
 breaker = list(guess_me)
-str = ''
+str = ' '
 new_board = "_" * len(breaker)
 scoreboard = list(new_board)
 WRONG = []
 past_guesses = []
 
 def play_game():
-    print(f"{str.join(scoreboard)} \n GUESS ME!")
     if len(WRONG) == 8:
         print(f"LOSER!\nThe Word Was: {guess_me}")
         sys.exit(0)
     if scoreboard == breaker:
-        print("YOU WIN!")
+        print(f"{guess_me}\nYOU WIN!")
         sys.exit(0)
-    guess = input("Pick A Letter: ")
-    if guess[0] in past_guesses:
-        print("You Already Tried That...")
-        play_game()
-    elif guess[0] in breaker:
-        print("good guess!")
-        past_guesses.append(guess[0])
-        guess_list = guess[0]
-        for correct in range(len(breaker)):
-            if breaker[correct] == guess_list:
-                scoreboard[correct] = breaker[correct]
-        play_game()
-    elif guess[0] not in breaker:
-        print("dumbass")
-        past_guesses.append(guess[0])
-        WRONG.append("X")
-        print("X" * len(WRONG))
-        play_game()
+    else:
+        print(f"{str.join(scoreboard)} \n GUESS ME!")
+        guess = input("Pick A Letter: ")
+        if not guess.isalpha():
+            print("Letters only!")
+            play_game()
+        elif guess[0] in past_guesses:
+            print("You Already Tried That...")
+            play_game()
+        elif guess[0] in breaker:
+            print("good guess!")
+            past_guesses.append(guess[0])
+            guess_list = guess[0]
+            for correct in range(len(breaker)):
+                if breaker[correct] == guess_list:
+                    scoreboard[correct] = breaker[correct]
+            play_game()
+        elif guess[0] not in breaker:
+            print("dumbass")
+            past_guesses.append(guess[0])
+            WRONG.append("X")
+            print(" X" * len(WRONG),"\n",f'{8 - len(WRONG)} {"guess" if 8 - len(WRONG) == 1 else "guesses"} left!')
+            play_game()
 
 
 
